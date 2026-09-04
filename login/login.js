adaf3d60b77626130ddaefab39eb6fabe92a4a68
MARIA CLARA RAMOS BOSSETTO <52860545875@info.local>
2026-09-04 17:26:36 -0300

    :sparkles: Novo Recurso
    Feita toda pagina de login e adicionada arquivo JavaScript

---
+++ login/login.js
@@ -0,0 +1,11 @@
+// var nome = "Maria" --> Não utilizar
+// const nome = "Maria"; --> não troca de valor
+// let nome = "Maria";
+// console.log(nome) = print
+
+const campoSenha = document.getElementById("senha");
+const btnSenha = document.querySelector("#mostrar-senha");
+
+btnSenha.addEventListener("click",function() {
+    alert("Cliquei no botão!");
+})
