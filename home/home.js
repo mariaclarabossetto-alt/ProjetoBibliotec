c3f917ba9d46873eff534ad18838e2b9b7dc7579
MARIA CLARA RAMOS BOSSETTO <52860545875@info.local>
2026-09-11 17:24:27 -0300

    :sparkles: FEAT: Finalizada responsividade

---
+++ home/home.js
@@ -0,0 +1,15 @@
+const menu = document.getElementById("menu"); //pegando o elemento pelo id
+
+
+//estamos pegando o elemento de forma genérica - pode ser classe (precisa trazer o ponto) ou pode ser ID precisa trazer a #
+const navMenu = document.querySelector(".navegacao");
+
+menu.addEventListener("click", function() {
+    if(navMenu.className == "navegacao"){
+        navMenu.className = "navegacao ativo"
+    }
+
+    else{
+        navMenu.className = "navegacao";
+    }
+})
