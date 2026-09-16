3e54caf10979a9d9cbc532d62631ca40c08de821
MARIA CLARA RAMOS BOSSETTO <52860545875@info.local>
2026-09-16 17:21:39 -0300

    :sparkles: FEAT: Adicionado arquivo js do listagem de reservas

---
+++ visaoAdm/listagemReservas/listagemR.js
@@ -0,0 +1,23 @@
+const checboxes = document.querySelectorAll('input[type="checkbox"]');
+const statusRetirada = document.querySelectorAll(".status")
+
+checboxes.forEach((caixinha, indice, lista)=> {
+
+    // // corpo da função
+    // console.log("caixinha",caixinha)
+    // console.log("indice",indice)
+    // console.log("lista",lista)
+
+    caixinha.addEventListener("change",()=>{
+        if(caixinha.checked == true){
+            statusRetirada[indice].classList.remove("aguardando")
+            statusRetirada[indice].classList.add("retirado");
+            statusRetirada[indice].textContent = "Retirado";
+        }
+        else{
+            statusRetirada[indice].classList.add("aguardando")
+            statusRetirada[indice].classList.remove("retirado");
+            statusRetirada[indice].textContent = "Aguardando retirada";
+        }
+    })
+})
